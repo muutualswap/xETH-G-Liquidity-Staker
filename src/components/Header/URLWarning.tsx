fdf61d6dbc35299b9ae827ad4b8395d3b6ad41b6
muutualswap <75495839+muutualswap@users.noreply.github.com>
2020-12-29 17:15:30 -0500

    Update URLWarning.tsx

--- src/components/Header/URLWarning.tsx
+++ src/components/Header/URLWarning.tsx
@@ -30,15 +30,15 @@ export default function URLWarning() {
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>StakeXETH.finance</code>
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>Bigswap.app</code>
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
     </PhishAlert>
-  ) : window.location.hostname === 'app.uniswap.org' ? (
+  ) : window.location.hostname === 'bigswap.app' ? (
     <PhishAlert isActive={showURLWarning}>
       <div style={{ display: 'flex' }}>
         <AlertTriangle style={{ marginRight: 6 }} size={12} /> Always make sure the URL is
-        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>StakeXETH.finance</code> - bookmark it
+        <code style={{ padding: '0 4px', display: 'inline', fontWeight: 'bold' }}>Bigswap.app</code> - bookmark it
         to be safe.
       </div>
       <StyledClose size={12} onClick={toggleURLWarning} />
